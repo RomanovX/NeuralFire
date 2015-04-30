48241b3f505efc64eea5b590e10b750a986d186b
Gabriel Schanner <gabriel.schanner@student.tugraz.at>
2015-04-30 17:35:11 +0200

    implemented environment parsing using images

---
+++ Swarm_Sim/src/Constants.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+
+
+public class Constants {
+	
+	public static Color droidColor = Color.green;
+	public static Color fireColor = Color.red;
+	public static Color wallColor = Color.black;
+	public static Color floorColor = Color.white;
+	public static int preferredSquareSize = 10;
+}
