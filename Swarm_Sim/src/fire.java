27a92af194f5950c8542806a278733d420a8f225
Remy <remykabel.tu@gmail.com>
2015-05-01 03:37:28 +0200

    Draft implementation of LOS fire

--- Swarm_Sim/src/fire.java
+++ Swarm_Sim/src/fire.java
@@ -8,4 +8,35 @@ public class fire extends object{
 	{
 		stackable = true;
 	}
+	
+	public static void intRaytrace(int x0, int y0, int x1, int y1, Grid grid) 
+	{
+        int dx = Math.abs(x1 - x0);
+        int dy = Math.abs(y1 - y0);
+        int x = x0;
+        int y = y0;
+        int n = 1 + dx + dy;
+        int x_inc = (x1 > x0) ? 1 : -1;
+        int y_inc = (y1 > y0) ? 1 : -1;
+        int error = dx - dy;
+
+        dx *= 2;
+        dy *= 2;
+
+        for (; n > 0; --n) {
+        	grid.getField(y, x);
+
+            if (error > 0) {
+                x += x_inc;
+                error -= dy;
+            } else if (error < 0) {
+                y += y_inc;
+                error += dx;
+            } else {
+                x += x_inc;
+                y += y_inc;
+                n--;
+            }
+        }
+    }
 }
